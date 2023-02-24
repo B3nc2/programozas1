a56f6e9a83f40638d63c41564c90b98b4f6095aa
csehely <bencecsehely@gmail.com>
2023-02-24 15:31:26 +0100

    Add files via upload

---
+++ proghazi/oraimunkesz/masodikora.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+   /* int beker = 0;
+    printf("Adjon meg egy szamot:");
+    scanf("%d",&beker);
+    if(beker>0){
+        printf("A szam pozitiv");
+    }
+    else if (beker < 0){
+        printf("A szam negativ");
+    }
+    else{
+        printf("A szam 0");
+    }
+    */
+
+   /*for(int i=0; i < 5; i++){
+    printf("Bence\n");
+   }
+   */
+
+  /*int x = 1234;
+  int a = 0;
+  int ford = 0;
+  while(x != 0){
+    a = x%10;
+    x /= 10;
+    ford = ford * 10 + a;
+    
+  }
+  while(ford != 0){
+    a = ford%10;
+    ford /= 10;
+    printf(", %d",a);
+  }
+  */
+/*int i = 0;
+int j = 10;
+ for( i = 0; i<10; i++){
+    printf("%d ",i);
+ }
+ printf("\n");
+ for(j = 10; j>0; j--){
+    printf("%d ",j);
+ }
+printf("\n");
+ i = 0;
+ j = 10;
+ while(i < 10){
+  printf("%d ",i);
+  i++;
+ }
+ printf("\n");
+ while(j > 0){
+    printf("%d ",j);
+    j--;
+ }
+ */
+   /*
+    int hossz = 0;
+    printf("Adja meg hanyszor irassa ki:");
+    scanf("%d", &hossz);
+    for(int i = 1; i <= hossz; i++){
+        printf("%d) Hello world!\n", i);
+    }
+    */
+  /*int tarol = 0;
+   for(int i = 1; i <= 100; i++){
+    tarol += i;
+
+   }
+   printf("%d", tarol);
+    
+    */
+
+   /*const int N = 1000;
+   int x = 0;
+   for( int i = 0; i < N; i++){
+    if(i % 3 == 0 || i % 5 == 0){
+        x += i;
+    }
+   }
+     printf("%d",x);
+    */
+
+   /*int x = 0;
+   
+   do{
+    printf("Adjon meg egy pozitiv egesz szamot:");
+    scanf("%d", &x);
+    if(x<=0){
+        printf("A szam nem pozitiv\n");
+    }
+   }while(x < 0);
+   */
+    return 0;
+}
